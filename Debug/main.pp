4e9cdee62c64fe7f67ba5682a0bb8f2d5f3c8ea7
[TUC]Theppasith N <tutorgaming@gmail.com>
2015-07-21 16:06:16 +0200

    First Initial (Working Solution)

---
+++ Debug/main.pp
@@ -0,0 +1,23 @@
+# FIXED
+
+main.obj: ../main.c
+main.obj: C:/ti/ccsv6/ccs_base/msp430/include/msp430f5529.h
+main.obj: C:/ti/ccsv6/ccs_base/msp430/include/in430.h
+main.obj: C:/ti/ccsv6/tools/compiler/ti-cgt-msp430_4.4.3/include/intrinsics.h
+main.obj: C:/ti/ccsv6/tools/compiler/ti-cgt-msp430_4.4.3/include/intrinsics_legacy_undefs.h
+main.obj: C:/ti/ccsv6/tools/compiler/ti-cgt-msp430_4.4.3/include/stdio.h
+main.obj: C:/ti/ccsv6/tools/compiler/ti-cgt-msp430_4.4.3/include/linkage.h
+main.obj: C:/ti/ccsv6/tools/compiler/ti-cgt-msp430_4.4.3/include/stdarg.h
+main.obj: C:/ti/ccsv6/tools/compiler/ti-cgt-msp430_4.4.3/include/stdlib.h
+main.obj: C:/ti/ccsv6/tools/compiler/ti-cgt-msp430_4.4.3/include/string.h
+
+../main.c: 
+C:/ti/ccsv6/ccs_base/msp430/include/msp430f5529.h: 
+C:/ti/ccsv6/ccs_base/msp430/include/in430.h: 
+C:/ti/ccsv6/tools/compiler/ti-cgt-msp430_4.4.3/include/intrinsics.h: 
+C:/ti/ccsv6/tools/compiler/ti-cgt-msp430_4.4.3/include/intrinsics_legacy_undefs.h: 
+C:/ti/ccsv6/tools/compiler/ti-cgt-msp430_4.4.3/include/stdio.h: 
+C:/ti/ccsv6/tools/compiler/ti-cgt-msp430_4.4.3/include/linkage.h: 
+C:/ti/ccsv6/tools/compiler/ti-cgt-msp430_4.4.3/include/stdarg.h: 
+C:/ti/ccsv6/tools/compiler/ti-cgt-msp430_4.4.3/include/stdlib.h: 
+C:/ti/ccsv6/tools/compiler/ti-cgt-msp430_4.4.3/include/string.h: 
---
+++ main.c
@@ -0,0 +1,254 @@
+#include <msp430f5529.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/*
+ * main.c
+ */
+typedef struct _som_node_{
+	double elements[4];
+} Node;
+typedef struct _data_{
+	double attribute[4];
+	int tag;
+} Data;
+/*************************************
+ * MSP430 Variable
+ *************************************/
+	// Buffer for serial receiver
+	char 				inputArray[11];
+	// All purpose iterator
+	volatile int 		it  			= 0;
+	volatile int		itt				= 0;
+	volatile int		ptr				= 0; //Pointer for serial input
+	volatile int 		input_enable	= 1; //Input Enable for Serial comm
+/*************************************
+ * Global constant and Variable
+ *************************************/
+	unsigned const int  ROW             = 15;
+    unsigned const int  COL             = 15;
+    unsigned int        iteration_count = 0;
+    const int           element_count   = 4;
+    //int                 class_count     = 3; // IRIS DATASET = 3
+	int 				x				= 0;
+	int 				y				= 0;
+	int 				result			= 0;
+/*************************************
+ * Data Structure for a node
+ *************************************/
+
+	/*double 			data[line_count][element_count];
+	double 				real[line_count][element_count];
+	int    				class[line_count];*/
+
+/*************************************
+ * Function
+ *************************************/
+
+//UART SERIAL COMMUNICATION //////////////////
+void uart_putchar(char input){
+	while (!(UCA1IFG&UCTXIFG));
+	UCA1TXBUF = input;
+}
+void uart_newline(){
+	uart_putchar('\n');
+	uart_putchar('\r');
+}
+void println(char *input , int input_size){
+	int i = input_size;
+	for(; i>0 ; i--)uart_putchar(input[input_size-i]);
+		uart_putchar('\n');
+		uart_putchar('\r');
+}
+
+void sendACK(){
+	uart_putchar('@');
+	uart_newline();
+	ptr=0;
+	input_enable = 1;
+		//CLEAR BUFFER FOR NEXT INPUT
+		inputArray[0] = ' ';
+		inputArray[1] = ' ';
+		inputArray[2] = ' ';
+		inputArray[3] = ' ';
+		inputArray[4] = ' ';
+		inputArray[5] = ' ';
+		inputArray[6] = ' ';
+		inputArray[7] = ' ';
+		inputArray[8] = ' ';
+		inputArray[9] = ' ';
+		inputArray[10] = ' ';
+		//TURN LED OFF
+		P1OUT = 0b00000000;
+}
+
+
+void initialize(){
+	 // P3.3,4 = USCI_A1 TXD/RXD (USB TX RX on PORT 3.3,3.4)
+	 P4SEL |= BIT5+BIT4;
+	 // Reset UCA1CTL1
+	 UCA1CTL1 |= UCSWRST;
+	 // Select Source For UART => SMCLK (1.5Mhz)
+	 UCA1CTL1 |= UCSSEL_3;
+	 // Set Baud rate
+	 UCA1BR1 = 0x00; UCA1BR0 = 0x6D; // 1.5MHz/9600=0x006D
+	 UCA1MCTL |= UCBRS_2 + UCBRF_0; // Modulation UCBRSx=2, UCBRFx=0 (FROM DATASHEET)
+	 UCA1CTL1 &= ~UCSWRST; // Initialize the state machine
+	 // Enable USCI_A1 RX interrupt
+	 UCA1IE |= UCRXIE;
+	 //LED Initialization
+	 P1DIR |= 0x01;
+	 P4DIR |= 0x80;
+	 P1OUT = 0b00000000;
+	 P4OUT = 0b00000000;
+}
+
+double euclidian_distance_point_square(int x1, int y1 , int x2 , int y2 ){
+	return (x1-x2)*(x1-x2) + (y1-y2)*(y1-y2);
+}
+
+double euclidian_distance_vector_squre(double input1[] , double input2[]){
+	int i;
+	double dist;
+	for(i = 0 ; i < element_count ; i++){
+		dist += (input1[i] - input2[i])*(input1[i] - input2[i]);
+	}
+	return dist;
+
+}
+
+
+/*************************************
+ * Main Function
+ *************************************/
+int main(void) {
+	// Stop watchdog timer
+    	WDTCTL = WDTPW | WDTHOLD;
+    // Initialize Ports
+    	initialize();
+	// Receiver Interrupt
+		__bis_SR_register(GIE); //interrupts enabled
+		__no_operation();
+////////////////////////////////////////////////////////////////////
+		// Receive RAW Data via serial
+			Node som_map[15][15];
+		    int plotty[15][15];
+			int i , j , e;
+			//Trained Network Data
+				for(i = 0 ; i < ROW ; i++){
+					for(j = 0 ; j < COL ; j++){
+						for(e=0 ; e < element_count ; e++){
+							while(input_enable == 1);
+								som_map[i][j].elements[e] = atof(inputArray);
+							sendACK();
+						}
+					}
+				}
+			//Classification Array (position of trained data)
+					for(i = 0 ; i < ROW ; i++){
+						for(j = 0 ; j < COL ; j++){
+								while(input_enable == 1);
+									plotty[i][j] = atoi(inputArray);
+								sendACK();
+							}
+					}
+		//GREEN LED SHOW THAT MODEL HAS FINISHED LOADING
+			P4OUT = 0b10000000;
+			P1OUT = 0b00000000;
+
+		//Q&A Process
+		//double question[element_count];
+		double minimum = 10000;
+		double temp = 0;
+//		double a1 = 0;
+//		double a2 = 0;
+//		double a3 = 0;
+//		double a4 = 0;
+		double question[element_count];
+
+		while(1){
+			P1OUT = 0b00000000;
+			//Receive Question
+//			while (input_enable == 1);
+//				a1 = atof(inputArray);
+//			sendACK();
+//			while (input_enable == 1);
+//				a2 = atof(inputArray);
+//			sendACK();
+//			while (input_enable == 1);
+//				a3 = atof(inputArray);
+//			sendACK();
+//			while (input_enable == 1);
+//				a4 = atof(inputArray);
+//			sendACK();
+			for(e = 0 ; e < element_count ; e++){
+				while (input_enable == 1);
+					question[e] = atof(inputArray);
+				sendACK();
+			}
+
+//				for (i = 0; i < ROW; i++) {
+//					for (j = 0; j < COL; j++) {
+//						temp = (a1 - som_map[i][j].elements[0])*(a1 - som_map[i][j].elements[0]) +
+//								(a2 - som_map[i][j].elements[1])*(a2 - som_map[i][j].elements[1]) +
+//								(a3 - som_map[i][j].elements[2])*(a3 - som_map[i][j].elements[2]) +
+//								(a4 - som_map[i][j].elements[3])*(a4 - som_map[i][j].elements[3]);
+//						if (temp < minimum) {
+//							minimum = temp;
+//							x = j;
+//							y = i;
+//						}
+//					}
+//				}
+//				minimum = 10000;
+//				temp = 0;
+//
+//				for (i = 0; i < ROW; i++) {
+//					for (j = 0; j < COL; j++) {
+//						if(plotty[i][j] != 0 ){
+//							temp = euclidian_distance_point_square(x,y,j,i);
+//							if(temp < minimum){
+//								result = plotty[i][j];
+//								minimum = temp;
+//							}
+//						}
+//					}
+//				}
+//
+//				//GREEN LED SHOW THAT MODEL HAS FINISHED LOADING
+//				P4OUT = 0b10000000;
+//				P1OUT = 0b00000001;
+//				minimum = 10000;
+//				temp = 0;
+//				x=0;
+//				y=0;
+//				a1=0;
+//				a2=0;
+//				a3=0;
+//				a4=0;
+		}
+
+
+
+}
+
+
+// RECEIVER ( INTERRUPT ON RECEIVE )
+// This is the RX UART ISR and is entered when the RX buffer is full
+#pragma vector=USCI_A1_VECTOR
+__interrupt void USCI_A1_ISR(void){
+	//Input
+	while (!(UCA1IFG&UCTXIFG)); // Is the USCI_A0 TX buffer ready?
+		//Return text to Screen
+	UCA1TXBUF = UCA1RXBUF;
+	if(input_enable){
+		if(UCA1RXBUF == ','){
+			input_enable = 0;
+			P1OUT = 0b00000001;
+		}else{
+			inputArray[ptr++] = UCA1RXBUF;
+		}
+	}else{
+		P1OUT = 0b00000000;
+	}
+}
